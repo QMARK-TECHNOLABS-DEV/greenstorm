0a18990c797d97a62928aa836058aa1c813179c2
Qmark Technolabs <141575937+Qmark-Technolabs@users.noreply.github.com>
2025-01-31 15:09:20 +0530

    Update price-section.blade.php

--- resources/views/price-section.blade.php
+++ resources/views/price-section.blade.php
@@ -121,9 +121,9 @@
     <div class="container mt-5 mb-5"> <!-- Added mb-5 for margin-bottom -->
     <div class="row">
         <div class="col-lg-12 text-center">
-            <!-- Note -->
-            <h5 class="mb-4" style="color: #333; font-size: 16px; font-weight: 500;">
-                Note: Submission ends on <strong>28 February 2025</strong>
+            <!-- Submission Deadline -->
+            <h5 class="mb-4" style="color: #333; font-size: 20px; font-weight: bold; margin-top: -15px;">
+                Submission ends on <strong>28 February 2025</strong>
             </h5>
             <!-- CTA Button -->
             <a href="{{ url('profile/upload-photograph') }}" class="submit-btn" style="background: linear-gradient(45deg, #b0d232, #36b348); color: #fff; padding: 15px 30px; border-radius: 50px; text-decoration: none; font-size: 18px; font-weight: bold; border: 3px solid #badcb2; box-shadow: 1px 3px 5px rgba(211, 224, 255, 0.5);">
@@ -132,4 +132,5 @@
         </div>
     </div>
 </div>
+
 </section>
