3c8f1e4a98cc1d67b4c9f76767b48d98ce1bc8f1
Qmark Technolabs <141575937+Qmark-Technolabs@users.noreply.github.com>
2025-01-31 15:15:18 +0530

    Update price-section.blade.php

--- resources/views/price-section.blade.php
+++ resources/views/price-section.blade.php
@@ -126,7 +126,7 @@
                 Submission ends on <strong>28 February 2025</strong>
             </h5>
             <!-- CTA Button -->
-            <a href="{{ url('profile/upload-photograph') }}" class="submit-btn" style="background: linear-gradient(45deg, #b0d232, #36b348); color: #fff; padding: 15px 30px; border-radius: 50px; text-decoration: none; font-size: 18px; font-weight: bold; border: 3px solid #badcb2; box-shadow: 1px 3px 5px rgba(211, 224, 255, 0.5); transform: translateY(-50px);">
+            <a href="{{ url('profile/upload-photograph') }}" class="submit-btn" style="background: linear-gradient(45deg, #b0d232, #36b348); color: #fff; padding: 15px 30px; border-radius: 50px; text-decoration: none; font-size: 18px; font-weight: bold; border: 3px solid #badcb2; box-shadow: 1px 3px 5px rgba(211, 224, 255, 0.5); transform: translateY(-80px);">
                 Submit
             </a>
         </div>
