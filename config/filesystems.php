3f2348708ff371183fe6eeb1467bae5738d18066
amritha anil <amrithaanil2018@gmail.com>
2025-02-02 02:54:37 +0530

    fs1

--- config/filesystems.php
+++ config/filesystems.php
@@ -57,13 +57,14 @@ return [
         ],
 
         'azure' => [
+            
             'driver'    => 'azure',
             'name'      => env('AZURE_STORAGE_NAME'),
             'key'       => env('AZURE_STORAGE_KEY'),
             'container' => env('AZURE_STORAGE_CONTAINER'),
             'url'       => env('AZURE_STORAGE_URL'),
             'prefix'    => null,
-            
+
         ],
 
     ],
