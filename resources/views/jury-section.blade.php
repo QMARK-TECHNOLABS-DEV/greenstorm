ab6a0f18fda34cc56b4b5bd8ffdf0e303c461417
Qmark Technolabs <141575937+Qmark-Technolabs@users.noreply.github.com>
2025-03-20 11:31:40 +0530

    Update jury-section.blade.php

--- resources/views/jury-section.blade.php
+++ resources/views/jury-section.blade.php
@@ -1,109 +1,183 @@
 <style>
-.jiimage
-{
-height:545px !important; 
-object-fit:cover;
+.jiimage {
+    height: 545px !important;
+    object-fit: cover;
 }
-    
 </style>
 
 <section class="team-area pt-60 pb-60">
     <div class="container">
         <div class="row">
             <div class="col-xl-4 col-sm-4 col-sm-12 wow animate__animated animate__fadeInLeft" data-wow-delay=".3s">
-                <h2 class="sec-ttl border_btm  text-green text-capitalize"> Meet <br> the Jury</h2>
+                <h2 class="sec-ttl border_btm text-green text-capitalize">Meet <br> the Jury</h2>
             </div>
-            <div class="col-xl-8 col-md-8 col-sm-12 wow animate__animated animate__fadeInRight"
-                data-wow-delay=".3s">
+            <div class="col-xl-8 col-md-8 col-sm-12 wow animate__animated animate__fadeInRight" data-wow-delay=".3s">
                 <p>The Greenstorm Global Photography Festival proudly welcomes a distinguished Global jury, featuring pioneers in photography and environmental storytelling. These experts bring together the worlds of art, conservation, and education, reflecting the core values of the Greenstorm.</p>
             </div>
         </div>
     </div>
+
     <div class="container-fluid pt-100 wow animate__animated animate__fadeInUp" data-wow-delay=".3s">
         <div class="row">
             <div class="team-slides owl-carousel owl-theme">
+                <!-- Existing Jury Members -->
                 <div class="item">
-                    <div class=" team_clm ">
+                    <div class="team_clm">
                         <div class="single-team-box">
                             <div class="image">
                                 <img class="jiimage" src="{{ asset('web/img/team/jury/charlie-waite.jpg') }}" alt="image">
                             </div>
                             <div class="content">
-                                <h3>Charlie Waite  </h3>
-                                <span>United Kingdom </span>
+                                <h3>Charlie Waite</h3>
+                                <span>United Kingdom</span>
                                 <p class="text-center">
-                                    <a data-bs-toggle="modal" data-bs-target="#teamOne" href="#"
-                                        class="team-btn text-uppercase ">
-                                    <span> Read More</span>
-                                    <span class="icon"><img src="{{ asset('web/img/right-arrow.svg') }}"> </span>
+                                    <a data-bs-toggle="modal" data-bs-target="#teamOne" href="#" class="team-btn text-uppercase">
+                                        <span>Read More</span>
+                                        <span class="icon"><img src="{{ asset('web/img/right-arrow.svg') }}"></span>
                                     </a>
                                 </p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <!-- item  end -->
-                <!--<div class="item">-->
-                <!--    <div class=" team_clm ">-->
-                <!--        <div class="single-team-box">-->
-                <!--            <div class="image">-->
-                <!--                <img src="{{ asset('web/img/team/jury/emily-graithwaite.jpg') }}" alt="image">-->
-                <!--            </div>-->
-                <!--            <div class="content">-->
-                <!--                <h3>Emily Graithwaite</h3>-->
-                <!--                <span>Iraq </span>-->
-                <!--                <p class="text-center">-->
-                <!--                    <a data-bs-toggle="modal" data-bs-target="#teamTwo" href="#"-->
-                <!--                        class="team-btn text-uppercase ">-->
-                <!--                    <span> Read More</span>-->
-                <!--                    <span class="icon"><img src="{{ asset('web/img/right-arrow.svg') }}"> </span>-->
-                <!--                    </a>-->
-                <!--                </p>-->
-                <!--            </div>-->
-                <!--        </div>-->
-                <!--    </div>-->
-                <!--</div>-->
+
                 <div class="item">
-                    <div class=" team_clm">
+                    <div class="team_clm">
                         <div class="single-team-box">
                             <div class="image">
                                 <img class="jiimage" src="{{ asset('web/img/team/jury/latika-nath.jpg') }}" alt="image">
                             </div>
                             <div class="content">
-                                <h3>Latika Nath </h3>
-                                <span>India </span>
+                                <h3>Latika Nath</h3>
+                                <span>India</span>
                                 <p class="text-center">
-                                    <a data-bs-toggle="modal" data-bs-target="#teamThree" href="#"
-                                        class="team-btn text-uppercase ">
-                                    <span> Read More</span>
-                                    <span class="icon"><img src="{{ asset('web/img/right-arrow.svg') }}"> </span>
+                                    <a data-bs-toggle="modal" data-bs-target="#teamThree" href="#" class="team-btn text-uppercase">
+                                        <span>Read More</span>
+                                        <span class="icon"><img src="{{ asset('web/img/right-arrow.svg') }}"></span>
                                     </a>
                                 </p>
                             </div>
                         </div>
                     </div>
                 </div>
+
                 <div class="item">
-                    <div class=" team_clm">
+                    <div class="team_clm">
                         <div class="single-team-box">
                             <div class="image">
-                                <img class="jiimage" src="{{ asset('web/img/team/jury/nick.jpg') }}" alt="image" style="">
+                                <img class="jiimage" src="{{ asset('web/img/team/jury/nick.jpg') }}" alt="image">
                             </div>
                             <div class="content">
                                 <h3>Nick Hall</h3>
                                 <span>USA</span>
                                 <p class="text-center">
-                                    <a data-bs-toggle="modal" data-bs-target="#teamFour" href="#"
-                                        class="team-btn text-uppercase ">
-                                    <span> Read More</span>
-                                    <span class="icon"><img src="{{ asset('web/img/right-arrow.svg') }}"> </span>
+                                    <a data-bs-toggle="modal" data-bs-target="#teamFour" href="#" class="team-btn text-uppercase">
+                                        <span>Read More</span>
+                                        <span class="icon"><img src="{{ asset('web/img/right-arrow.svg') }}"></span>
                                     </a>
                                 </p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <!-- item  end -->
+                <!-- End Existing Jury Members -->
+            </div>
+        </div>
+    </div>
+
+    <!-- Jury 1 New Section -->
+    <div class="container-fluid pt-100 wow animate__animated animate__fadeInUp" data-wow-delay=".3s">
+        <div class="row">
+            <div class="col-12 mb-4">
+                <h2 class="sec-ttl border_btm text-green text-capitalize">Jury 1</h2>
+            </div>
+
+            <div class="team-slides owl-carousel owl-theme">
+                <!-- New Jury Members -->
+                <div class="item">
+                    <div class="team_clm">
+                        <div class="single-team-box">
+                            <div class="image">
+                                <img class="jiimage" src="{{ asset('web/img/team/jury/jury1.jpg') }}" alt="Jury 1">
+                            </div>
+                            <div class="content">
+                                <h3>Jury Member 1</h3>
+                                <span>Country 1</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <div class="team_clm">
+                        <div class="single-team-box">
+                            <div class="image">
+                                <img class="jiimage" src="{{ asset('web/img/team/jury/jury2.jpg') }}" alt="Jury 2">
+                            </div>
+                            <div class="content">
+                                <h3>Jury Member 2</h3>
+                                <span>Country 2</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <div class="team_clm">
+                        <div class="single-team-box">
+                            <div class="image">
+                                <img class="jiimage" src="{{ asset('web/img/team/jury/jury3.jpg') }}" alt="Jury 3">
+                            </div>
+                            <div class="content">
+                                <h3>Jury Member 3</h3>
+                                <span>Country 3</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <div class="team_clm">
+                        <div class="single-team-box">
+                            <div class="image">
+                                <img class="jiimage" src="{{ asset('web/img/team/jury/jury4.jpg') }}" alt="Jury 4">
+                            </div>
+                            <div class="content">
+                                <h3>Jury Member 4</h3>
+                                <span>Country 4</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <div class="team_clm">
+                        <div class="single-team-box">
+                            <div class="image">
+                                <img class="jiimage" src="{{ asset('web/img/team/jury/jury5.jpg') }}" alt="Jury 5">
+                            </div>
+                            <div class="content">
+                                <h3>Jury Member 5</h3>
+                                <span>Country 5</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <div class="team_clm">
+                        <div class="single-team-box">
+                            <div class="image">
+                                <img class="jiimage" src="{{ asset('web/img/team/jury/jury6.jpg') }}" alt="Jury 6">
+                            </div>
+                            <div class="content">
+                                <h3>Jury Member 6</h3>
+                                <span>Country 6</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- End New Jury Members -->
             </div>
         </div>
     </div>
