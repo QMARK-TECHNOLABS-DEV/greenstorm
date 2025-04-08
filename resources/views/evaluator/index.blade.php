34ac8c3c53f0f303185d681310f5fb2785aca49a
Qmark Technolabs <141575937+Qmark-Technolabs@users.noreply.github.com>
2025-04-09 00:49:11 +0530

    Update index.blade.php

--- resources/views/evaluator/index.blade.php
+++ resources/views/evaluator/index.blade.php
@@ -24,7 +24,7 @@
                 <div class="signin-logo tx-center tx-28 tx-bold tx-inverse"><span class="tx-normal"></span> Evaluator's <span class="tx-white">Dashboard</span> <span class="tx-normal"></span></div>
                 <div class="d-flex flex-column align-items-center p-4 mb-3">
     <!-- Text above logos -->
-<div class="mb-1 text-center" style="transform: translate(-27px, 13px); color: #52a25c;">
+<div class="mb-1 text-center" style="transform: translate(-27px, 13px); color: black;">
     <strong>In Association With</strong>
 </div>
 
