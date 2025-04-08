287c30e31794d60e5d2b487a4c324279f1cc2690
Qmark Technolabs <141575937+Qmark-Technolabs@users.noreply.github.com>
2025-04-08 23:48:02 +0530

    Update index.blade.php

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -24,9 +24,10 @@
                 <div class="signin-logo tx-center tx-28 tx-bold tx-inverse"><span class="tx-normal"></span> Admin <span class="tx-info">Dashboard</span> <span class="tx-normal"></span></div>
                <div class="d-flex flex-column align-items-center p-4 mb-3">
     <!-- Text above logos -->
-    <div class="mb-2 text-center">
-        <strong>In Association With</strong>
-    </div>
+<div class="mb-1 text-center" style="transform: translate(-10px, 5px);">
+    <strong>In Association With</strong>
+</div>
+
 
     <!-- Logos side by side -->
     <div class="d-flex align-items-center justify-content-center gap-3">
