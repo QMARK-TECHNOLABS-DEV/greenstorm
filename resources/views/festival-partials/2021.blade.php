92bf49f77f0d6b183a6871294aa9376d6c2049f4
Qmark Technolabs <141575937+Qmark-Technolabs@users.noreply.github.com>
2025-04-23 21:18:21 +0530

    Update 2021.blade.php

--- resources/views/festival-partials/2021.blade.php
+++ resources/views/festival-partials/2021.blade.php
@@ -2,7 +2,7 @@
     <li class="col-lg-4 col-md-6 ">
         <a class="image-popup-vertical-fit" href="{{ asset('web/img/gallery/2021/Alok-Avinash.jpg') }}" title="Alok-Avinash ">
             <div class='box-4'>
-                <figure class="imgLiquidFill imgLiquid gallery_image_thumb">
+                <figure >
                     <img src="{{ asset('web/img/gallery/2021/Alok-Avinash.jpg') }}" alt="" />
                 </figure>
                  <div class='lupa text-center'>
@@ -30,7 +30,7 @@
     <li class="col-lg-4 col-md-6 ">
         <a class="image-popup-vertical-fit" href="{{ asset('web/img/gallery/2021/Angel-M-Baby.jpg') }}" title="Angel-M-Baby ">
             <div class='box-4'>
-                <figure class="imgLiquidFill imgLiquid gallery_image_thumb">
+                <figure >
                     <img src="{{ asset('web/img/gallery/2021/Angel-M-Baby.jpg') }}" alt="" />
                 </figure>
                 <div class='lupa text-center'>
@@ -44,7 +44,7 @@
     <li class="col-lg-4 col-md-6 ">
         <a class="image-popup-vertical-fit" href="{{ asset('web/img/gallery/2021/Arpan-Basu-Chowdhury.jpg') }}" title="Arpan-Basu-Chowdhury ">
             <div class='box-4'>
-                <figure class="imgLiquidFill imgLiquid gallery_image_thumb">
+                <figure >
                     <img src="{{ asset('web/img/gallery/2021/Arpan-Basu-Chowdhury.jpg') }}" alt="" />
                 </figure>
                 <div class='lupa text-center'>
@@ -58,7 +58,7 @@
     <li class="col-lg-4 col-md-6 ">
         <a class="image-popup-vertical-fit" href="{{ asset('web/img/gallery/2021/Ashim-Kumar-Mukhopadhyay.jpg') }}" title="Ashim-Kumar-Mukhopadhyay">
             <div class='box-4'>
-                <figure class="imgLiquidFill imgLiquid gallery_image_thumb">
+                <figure >
                     <img src="{{ asset('web/img/gallery/2021/Ashim-Kumar-Mukhopadhyay.jpg') }}" alt="" />
                 </figure>
                 <div class='lupa text-center'>
@@ -72,7 +72,7 @@
     <li class="col-lg-4 col-md-6 ">
         <a class="image-popup-vertical-fit" href="{{ asset('web/img/gallery/2021/Binz-Jos.jpg') }}" title="Binz-Jos">
             <div class='box-4'>
-                <figure class="imgLiquidFill imgLiquid gallery_image_thumb">
+                <figure >
                     <img src="{{ asset('web/img/gallery/2021/Binz-Jos.jpg') }}" alt="" />
                 </figure>
                 <div class='lupa text-center'>
@@ -86,7 +86,7 @@
     <li class="col-lg-4 col-md-6 ">
         <a class="image-popup-vertical-fit" href="{{ asset('web/img/gallery/2021/Emman-Foronda.jpg') }}" title="Binz-Jos">
             <div class='box-4'>
-                <figure class="imgLiquidFill imgLiquid gallery_image_thumb">
+                <figure >
                     <img src="{{ asset('web/img/gallery/2021/Emman-Foronda.jpg') }}" alt="" />
                 </figure>
                 <div class='lupa text-center'>
@@ -100,7 +100,7 @@
     <li class="col-lg-4 col-md-6 ">
         <a class="image-popup-vertical-fit" href="{{ asset('web/img/gallery/2021/Froilan-Robas.jpg') }}" title="Binz-Jos">
             <div class='box-4'>
-                <figure class="imgLiquidFill imgLiquid gallery_image_thumb">
+                <figure >
                     <img src="{{ asset('web/img/gallery/2021/Froilan-Robas.jpg') }}" alt="" />
                 </figure>
                 <div class='lupa text-center'>
@@ -114,7 +114,7 @@
     <li class="col-lg-4 col-md-6 ">
         <a class="image-popup-vertical-fit" href="{{ asset('web/img/gallery/2021/Hadi-Dehghanpour.jpg') }}" title="Binz-Jos">
             <div class='box-4'>
-                <figure class="imgLiquidFill imgLiquid gallery_image_thumb">
+                <figure >
                     <img src="{{ asset('web/img/gallery/2021/Hadi-Dehghanpour.jpg') }}" alt="" />
                 </figure>
                 <div class='lupa text-center'>
@@ -128,7 +128,7 @@
     <li class="col-lg-4 col-md-6 ">
         <a class="image-popup-vertical-fit" href="{{ asset('web/img/gallery/2021/Jophel-Botero-Ybiosa.jpg') }}" title="Binz-Jos">
             <div class='box-4'>
-                <figure class="imgLiquidFill imgLiquid gallery_image_thumb">
+                <figure >
                     <img src="{{ asset('web/img/gallery/2021/Jophel-Botero-Ybiosa.jpg') }}" alt="" />
                 </figure>
                 <div class='lupa text-center'>
@@ -142,7 +142,7 @@
     <li class="col-lg-4 col-md-6 ">
         <a class="image-popup-vertical-fit" href="{{ asset('web/img/gallery/2021/Joy-Saha.jpg') }}" title="Binz-Jos">
             <div class='box-4'>
-                <figure class="imgLiquidFill imgLiquid gallery_image_thumb">
+                <figure >
                     <img src="{{ asset('web/img/gallery/2021/Joy-Saha.jpg') }}" alt="" />
                 </figure>
                 <div class='lupa text-center'>
@@ -156,7 +156,7 @@
     <li class="col-lg-4 col-md-6 ">
         <a class="image-popup-vertical-fit" href="{{ asset('web/img/gallery/2021/K.A-Abdul-Khader.jpg') }}" title="Binz-Jos">
             <div class='box-4'>
-                <figure class="imgLiquidFill imgLiquid gallery_image_thumb">
+                <figure >
                     <img src="{{ asset('web/img/gallery/2021/K.A-Abdul-Khader.jpg') }}" alt="" />
                 </figure>
                 <div class='lupa text-center'>
@@ -171,7 +171,7 @@
     <li class="col-lg-4 col-md-6 ">
         <a class="image-popup-vertical-fit" href="{{ asset('web/img/gallery/2021/Kallol-Mukherjee.jpg') }}" title="Binz-Jos">
             <div class='box-4'>
-                <figure class="imgLiquidFill imgLiquid gallery_image_thumb">
+                <figure >
                     <img src="{{ asset('web/img/gallery/2021/Kallol-Mukherjee.jpg') }}" alt="" />
                 </figure>
                 <div class='lupa text-center'>
@@ -186,7 +186,7 @@
     <li class="col-lg-4 col-md-6 ">
         <a class="image-popup-vertical-fit" href="{{ asset('web/img/gallery/2021/Kyaw-Thu.jpg') }}" title="Binz-Jos">
             <div class='box-4'>
-                <figure class="imgLiquidFill imgLiquid gallery_image_thumb">
+                <figure >
                     <img src="{{ asset('web/img/gallery/2021/Kyaw-Thu.jpg') }}" alt="" />
                 </figure>
                 <div class='lupa text-center'>
@@ -201,7 +201,7 @@
     <li class="col-lg-4 col-md-6 ">
         <a class="image-popup-vertical-fit" href="{{ asset('web/img/gallery/2021/Mahesh-Mallinath-Kamble.jpg') }}" title="Binz-Jos">
             <div class='box-4'>
-                <figure class="imgLiquidFill imgLiquid gallery_image_thumb">
+                <figure >
                     <img src="{{ asset('web/img/gallery/2021/Mahesh-Mallinath-Kamble.jpg') }}" alt="" />
                 </figure>
                 <div class='lupa text-center'>
@@ -216,7 +216,7 @@
     <li class="col-lg-4 col-md-6 ">
         <a class="image-popup-vertical-fit" href="{{ asset('web/img/gallery/2021/Mohamad-Reza-Masoumi.jpg') }}" title="Binz-Jos">
             <div class='box-4'>
-                <figure class="imgLiquidFill imgLiquid gallery_image_thumb">
+                <figure >
                     <img src="{{ asset('web/img/gallery/2021/Mohamad-Reza-Masoumi.jpg') }}" alt="" />
                 </figure>
                 <div class='lupa text-center'>
@@ -231,7 +231,7 @@
     <li class="col-lg-4 col-md-6 ">
         <a class="image-popup-vertical-fit" href="{{ asset('web/img/gallery/2021/Ratheesh-Narayanan.jpg') }}" title="Binz-Jos">
             <div class='box-4'>
-                <figure class="imgLiquidFill imgLiquid gallery_image_thumb">
+                <figure >
                     <img src="{{ asset('web/img/gallery/2021/Ratheesh-Narayanan.jpg') }}" alt="" />
                 </figure>
                 <div class='lupa text-center'>
@@ -246,7 +246,7 @@
     <li class="col-lg-4 col-md-6 ">
         <a class="image-popup-vertical-fit" href="{{ asset('web/img/gallery/2021/Shailie-Mehta.jpg') }}" title="Binz-Jos">
             <div class='box-4'>
-                <figure class="imgLiquidFill imgLiquid gallery_image_thumb">
+                <figure >
                     <img src="{{ asset('web/img/gallery/2021/Shailie-Mehta.jpg') }}" alt="" />
                 </figure>
                 <div class='lupa text-center'>
@@ -261,7 +261,7 @@
     <li class="col-lg-4 col-md-6 ">
         <a class="image-popup-vertical-fit" href="{{ asset('web/img/gallery/2021/Shibasish-Saha.jpg') }}" title="Binz-Jos">
             <div class='box-4'>
-                <figure class="imgLiquidFill imgLiquid gallery_image_thumb">
+                <figure >
                     <img src="{{ asset('web/img/gallery/2021/Shibasish-Saha.jpg') }}" alt="" />
                 </figure>
                 <div class='lupa text-center'>
@@ -276,7 +276,7 @@
     <li class="col-lg-4 col-md-6 ">
         <a class="image-popup-vertical-fit" href="{{ asset('web/img/gallery/2021/Shyjith-Onden-Cheriyath.jpg') }}" title="Binz-Jos">
             <div class='box-4'>
-                <figure class="imgLiquidFill imgLiquid gallery_image_thumb">
+                <figure >
                     <img src="{{ asset('web/img/gallery/2021/Shyjith-Onden-Cheriyath.jpg') }}" alt="" />
                 </figure>
                 <div class='lupa text-center'>
@@ -291,7 +291,7 @@
     <li class="col-lg-4 col-md-6 ">
         <a class="image-popup-vertical-fit" href="{{ asset('web/img/gallery/2021/sibin-bahuleyan.jpg') }}" title="Binz-Jos">
             <div class='box-4'>
-                <figure class="imgLiquidFill imgLiquid gallery_image_thumb">
+                <figure >
                     <img src="{{ asset('web/img/gallery/2021/sibin-bahuleyan.jpg') }}" alt="" />
                 </figure>
                 <div class='lupa text-center'>
@@ -306,7 +306,7 @@
     <li class="col-lg-4 col-md-6 ">
         <a class="image-popup-vertical-fit" href="{{ asset('web/img/gallery/2021/Sourav-Karmakar.jpg') }}" title="Binz-Jos">
             <div class='box-4'>
-                <figure class="imgLiquidFill imgLiquid gallery_image_thumb">
+                <figure >
                     <img src="{{ asset('web/img/gallery/2021/Sourav-Karmakar.jpg') }}" alt="" />
                 </figure>
                 <div class='lupa text-center'>
@@ -321,7 +321,7 @@
     <li class="col-lg-4 col-md-6 ">
         <a class="image-popup-vertical-fit" href="{{ asset('web/img/gallery/2021/Sujit-Saha.jpg') }}" title="Binz-Jos">
             <div class='box-4'>
-                <figure class="imgLiquidFill imgLiquid gallery_image_thumb">
+                <figure >
                     <img src="{{ asset('web/img/gallery/2021/Sujit-Saha.jpg') }}" alt="" />
                 </figure>
                 <div class='lupa text-center'>
@@ -336,7 +336,7 @@
     <li class="col-lg-4 col-md-6 ">
         <a class="image-popup-vertical-fit" href="{{ asset('web/img/gallery/2021/TITTU-SHAJI-THOMAS.jpg') }}" title="Binz-Jos">
             <div class='box-4'>
-                <figure class="imgLiquidFill imgLiquid gallery_image_thumb">
+                <figure >
                     <img src="{{ asset('web/img/gallery/2021/TITTU-SHAJI-THOMAS.jpg') }}" alt="" />
                 </figure>
                 <div class='lupa text-center'>
@@ -351,7 +351,7 @@
     <li class="col-lg-4 col-md-6 ">
         <a class="image-popup-vertical-fit" href="{{ asset('web/img/gallery/2021/Yuri-Pristik.jpg') }}" title="Binz-Jos">
             <div class='box-4'>
-                <figure class="imgLiquidFill imgLiquid gallery_image_thumb">
+                <figure >
                     <img src="{{ asset('web/img/gallery/2021/Yuri-Pristik.jpg') }}" alt="" />
                 </figure>
                 <div class='lupa text-center'>
