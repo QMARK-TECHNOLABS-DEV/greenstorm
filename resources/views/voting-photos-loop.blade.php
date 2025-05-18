4d78a86b5cab04675a0cc9c85567fa9e1c228c45
badusha <83378824+badusha5618@users.noreply.github.com>
2025-05-19 01:12:57 +0530

    Update voting-photos-loop.blade.php

--- resources/views/voting-photos-loop.blade.php
+++ resources/views/voting-photos-loop.blade.php
@@ -33,7 +33,10 @@
         @endif
     @else
 <p class="mt-2 text-white">
-   <a href="{{ route('login') }}" class="underline text-white"> Please login to vote</a> 
+   <a href="{{ route('login') }}?intended={{ url('/exhibition') }}" class="underline text-white">
+    Please login to vote
+</a>
+ 
 </p>
 
 
