1dc35d403d566b951e9c18fefbea6a365936d093
badusha <83378824+badusha5618@users.noreply.github.com>
2025-05-20 00:02:51 +0530

    Update voting-photos-loop.blade.php

--- resources/views/voting-photos-loop.blade.php
+++ resources/views/voting-photos-loop.blade.php
@@ -7,45 +7,38 @@
    data-ggpf-id="{{ $voting->photograph->photo_unique_id }}" 
    role="button">
 
-            <div class='votes_box'>
-                @if(Auth::check() && $voting->photograph->userVoted(Auth::user()->id))
-                <div class="corner-badge">
-                    <span class="fa fa-thumbs-up"></span>
-                </div>
-                @endif
-                <figure class="imgLiquidFill ">
-                    <img src="{{ $voting->photograph->image }}" alt="" />
-                </figure>
-              <div class='lupa text-center'>
-  
-  <p class="text-white text-sm mt-1">
-        Total Votes: {{ $voting->photograph->votes()->count() }}
-    </p>
-    @if(Auth::check())
-        @if(!$voting->photograph->userVoted(Auth::user()->id))
-<!--             <form method="POST" action="{{ route('vote.photo') }}">
-                @csrf
-                <input type="hidden" name="photo_id" value="{{ $voting->photo_id }}">
-                <button type="submit" class="btn btn-primary mt-2">Vote</button>
-            </form> -->
-        @else
-            <p class="text-success mt-2">You already voted</p>
-        @endif
-    @else
-<p class="mt-2 text-white">
-   <a href="{{ route('login') }}?intended={{ url('/exhibition') }}" class="underline text-white">
-    Please login to vote
-</a>
- 
-</p>
-
-
-
-
+         <div class='votes_box relative'> {{-- Add relative class --}}
+    @if(Auth::check() && $voting->photograph->userVoted(Auth::user()->id))
+    <div class="corner-badge">
+        <span class="fa fa-thumbs-up"></span>
+    </div>
     @endif
+
+    <figure class="imgLiquidFill">
+        <img src="{{ $voting->photograph->image }}" alt="" />
+    </figure>
+
+    <div class='lupa text-center absolute bottom-0 left-0 w-full bg-black bg-opacity-50 py-2'> {{-- Add absolute positioning --}}
+        <p class="text-white text-sm mt-1">
+            Total Votes: {{ $voting->photograph->votes()->count() }}
+        </p>
+
+        @if(Auth::check())
+            @if(!$voting->photograph->userVoted(Auth::user()->id))
+                {{-- Voting form (currently commented) --}}
+            @else
+                <p class="text-success mt-2">You already voted</p>
+            @endif
+        @else
+            <p class="mt-2 text-white">
+                <a href="{{ route('login') }}?intended={{ url('/exhibition') }}" class="underline text-white">
+                    Please login to vote
+                </a>
+            </p>
+        @endif
+    </div>
 </div>
 
-            </div>
         </a>
     </li>
     @endif
