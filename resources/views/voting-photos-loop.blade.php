a2d35a1671b1e41e52d39bb9cfb490afaf14120b
badusha <83378824+badusha5618@users.noreply.github.com>
2025-05-20 00:36:37 +0530

    Update voting-photos-loop.blade.php

--- resources/views/voting-photos-loop.blade.php
+++ resources/views/voting-photos-loop.blade.php
@@ -2,35 +2,32 @@
     @if($voting->photograph)
 
 
-
- <li class="col-lg-4 col-md-6 votingListingImgSection_{{ $voting->photo_id }} p-0 m-0" data-photo-id="{{ $voting->photo_id }}">
+<li class="col-lg-4 col-md-6 votingListingImgSection_{{ $voting->photo_id }} p-0 m-0" data-photo-id="{{ $voting->photo_id }}">
     <a class="imagePopupTriggerButton block w-full h-full" 
        data-photo-id="{{ $voting->photo_id }}"
        data-ggpf-id="{{ $voting->photograph->photo_unique_id }}"
        role="button">
 
         <div class="relative w-full h-full overflow-hidden">
-            {{-- Please login to vote (ABOVE image) --}}
+            {{-- Image --}}
+            <img src="{{ $voting->photograph->image }}" alt="" class="w-full h-auto block" />
+
+            {{-- "Please login to vote" INSIDE image, near top --}}
             @if(!Auth::check())
-                <div class="absolute top-4 left-1/2 transform -translate-x-1/2 text-white text-sm mb-2 z-20 text-center">
+                <div class="absolute top-4 left-1/2 transform -translate-x-1/2 text-white text-sm px-2 py-1 z-20 bg-black/50 rounded">
                     <a href="{{ route('login') }}?intended={{ url('/exhibition') }}" class="underline text-white">
                         Please login to vote
                     </a>
                 </div>
             @endif
 
-            {{-- Image --}}
-            <img src="{{ $voting->photograph->image }}" alt="" class="w-full h-auto block z-10" />
-
             {{-- Vote Count --}}
-            <div class="absolute bottom-10 left-1/2 transform -translate-x-1/2 text-white text-sm font-semibold z-20">
+            <div class="absolute bottom-4 left-1/2 transform -translate-x-1/2 text-white text-sm font-semibold z-20 bg-black/50 px-2 py-1 rounded">
                 Total Votes: {{ $voting->photograph->votes()->count() }}
             </div>
         </div>
     </a>
 </li>
-
-
 
 
     @endif
