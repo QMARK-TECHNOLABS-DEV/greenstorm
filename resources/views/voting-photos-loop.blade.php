7ab69072d8e304e8230036a7c76c0e7c16729ce4
badusha <83378824+badusha5618@users.noreply.github.com>
2025-05-20 00:34:44 +0530

    Update voting-photos-loop.blade.php

--- resources/views/voting-photos-loop.blade.php
+++ resources/views/voting-photos-loop.blade.php
@@ -1,34 +1,35 @@
 @foreach ($votingPhotos as $voting)
     @if($voting->photograph)
 
-       
-<li class="col-lg-4 col-md-6 votingListingImgSection_{{ $voting->photo_id }} p-0 m-0" data-photo-id="{{ $voting->photo_id }}">
+
+
+ <li class="col-lg-4 col-md-6 votingListingImgSection_{{ $voting->photo_id }} p-0 m-0" data-photo-id="{{ $voting->photo_id }}">
     <a class="imagePopupTriggerButton block w-full h-full" 
        data-photo-id="{{ $voting->photo_id }}"
        data-ggpf-id="{{ $voting->photograph->photo_unique_id }}"
        role="button">
 
         <div class="relative w-full h-full overflow-hidden">
+            {{-- Please login to vote (ABOVE image) --}}
+            @if(!Auth::check())
+                <div class="absolute top-4 left-1/2 transform -translate-x-1/2 text-white text-sm mb-2 z-20 text-center">
+                    <a href="{{ route('login') }}?intended={{ url('/exhibition') }}" class="underline text-white">
+                        Please login to vote
+                    </a>
+                </div>
+            @endif
+
             {{-- Image --}}
-            <img src="{{ $voting->photograph->image }}" alt="" class="w-full h-auto block" />
+            <img src="{{ $voting->photograph->image }}" alt="" class="w-full h-auto block z-10" />
 
             {{-- Vote Count --}}
-            <div class="absolute bottom-10 left-1/2 transform -translate-x-1/2 text-white text-sm font-semibold z-10">
+            <div class="absolute bottom-10 left-1/2 transform -translate-x-1/2 text-white text-sm font-semibold z-20">
                 Total Votes: {{ $voting->photograph->votes()->count() }}
             </div>
-
-            {{-- Login message (force visible) --}}
-           @if(!Auth::check())
-    <div class="absolute top-4 left-1/2 transform -translate-x-1/2 text-white text-sm mb-2 z-10">
-        <a href="{{ route('login') }}?intended={{ url('/exhibition') }}" class="underline text-white">
-            Please login to vote
-        </a>
-    </div>
-@endif
-
         </div>
     </a>
 </li>
+
 
 
 
