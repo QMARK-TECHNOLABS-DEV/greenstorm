f7c57b1e621770b02ecbaf2ebeaa76db6891bf3a
badusha <83378824+badusha5618@users.noreply.github.com>
2025-05-20 00:25:55 +0530

    Update voting-photos-loop.blade.php

--- resources/views/voting-photos-loop.blade.php
+++ resources/views/voting-photos-loop.blade.php
@@ -1,6 +1,6 @@
 @foreach ($votingPhotos as $voting)
     @if($voting->photograph)
-   <li class="col-lg-4 col-md-6 votingListingImgSection_{{ $voting->photo_id }} p-0 m-0" data-photo-id="{{ $voting->photo_id }}">
+<li class="col-lg-4 col-md-6 votingListingImgSection_{{ $voting->photo_id }} p-0 m-0" data-photo-id="{{ $voting->photo_id }}">
     <a class="imagePopupTriggerButton block w-full h-full" 
        data-photo-id="{{ $voting->photo_id }}"
        data-ggpf-id="{{ $voting->photograph->photo_unique_id }}"
@@ -9,13 +9,20 @@
         <div class="relative w-full h-full overflow-hidden">
             <img src="{{ $voting->photograph->image }}" alt="" class="w-full h-auto block" />
 
-            @if(Auth::check())
-                @if($voting->photograph->userVoted(Auth::user()->id))
-                    <div class="absolute top-0 right-0 m-2 bg-green-600 text-white text-xs px-2 py-1 rounded">
-                        <i class="fa fa-thumbs-up"></i>
-                    </div>
-                @endif
-            @else
+            {{-- Thumbs up badge if voted --}}
+            @if(Auth::check() && $voting->photograph->userVoted(Auth::user()->id))
+                <div class="absolute top-0 right-0 m-2 bg-green-600 text-white text-xs px-2 py-1 rounded">
+                    <i class="fa fa-thumbs-up"></i>
+                </div>
+            @endif
+
+            {{-- Total votes --}}
+            <div class="absolute bottom-8 left-1/2 transform -translate-x-1/2 text-white text-sm text-center">
+                Total Votes: {{ $voting->photograph->votes()->count() }}
+            </div>
+
+            {{-- Please login message --}}
+            @if(!Auth::check())
                 <div class="absolute bottom-2 left-1/2 transform -translate-x-1/2 text-white text-sm text-center">
                     <a href="{{ route('login') }}?intended={{ url('/exhibition') }}" class="underline text-white">
                         Please login to vote
@@ -25,6 +32,7 @@
         </div>
     </a>
 </li>
+
 
     @endif
 @endforeach 
