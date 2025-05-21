afd3d16da7365c9ce60da0339f276bab07e8b958
badusha <83378824+badusha5618@users.noreply.github.com>
2025-05-21 17:30:47 +0530

    Update voting-photos-loop.blade.php

--- resources/views/voting-photos-loop.blade.php
+++ resources/views/voting-photos-loop.blade.php
@@ -14,7 +14,7 @@
                 @endif
 
                 <!-- IMAGE BLOCK with LOWER HEIGHT -->
-                <div class="w-full h-[200px] overflow-hidden">
+                <div class="w-full h-[150px] overflow-hidden">
                     <img src="{{ $voting->photograph->image }}"
                          alt=""
                          class="w-full h-full object-cover block" />
