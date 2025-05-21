aa15c303954d9f1587fbf07a9eb45789ecd903f9
badusha <83378824+badusha5618@users.noreply.github.com>
2025-05-21 13:09:01 +0530

    Update voting-photos-loop.blade.php

--- resources/views/voting-photos-loop.blade.php
+++ resources/views/voting-photos-loop.blade.php
@@ -6,37 +6,33 @@
            data-ggpf-id="{{ $voting->photograph->photo_unique_id }}"
            role="button">
 
-            <div class="votes_box">
+            <div class="votes_box p-0 m-0 bg-transparent">
                 @if(Auth::check() && $voting->photograph->userVoted(Auth::user()->id))
                 <div class="corner-badge">
                     <span class="fa fa-thumbs-up"></span>
                 </div>
                 @endif
 
-                <figure class="w-full h-[300px] m-0 p-0 overflow-hidden">
-                    <img src="{{ $voting->photograph->image }}" alt="" class="w-full h-full object-cover block" />
-                </figure>
+                <!-- Image fully filling the container without gaps -->
+                <div class="w-full aspect-[4/3] relative overflow-hidden">
+                    <img src="{{ $voting->photograph->image }}" 
+                         alt="" 
+                         class="absolute top-0 left-0 w-full h-full object-cover" />
+                </div>
 
-                <div class="lupa text-center">
-                    <p class="text-white text-sm mt-1">
+                <div class="lupa text-center mt-2">
+                    <p class="text-white text-sm">
                         Total Votes: {{ $voting->photograph->votes()->count() }}
                     </p>
 
                     @if(Auth::check())
                         @if(!$voting->photograph->userVoted(Auth::user()->id))
-                            {{-- Uncomment to enable voting button --}}
-                            {{--
-                            <form method="POST" action="{{ route('vote.photo') }}">
-                                @csrf
-                                <input type="hidden" name="photo_id" value="{{ $voting->photo_id }}">
-                                <button type="submit" class="btn btn-primary mt-2">Vote</button>
-                            </form>
-                            --}}
+                            {{-- Vote button (disabled as per current logic) --}}
                         @else
                             <p class="text-success mt-2">You already voted</p>
                         @endif
                     @else
-                        <p class="mt-2 text-white">
+                        <p class="mt-2">
                             <a href="{{ route('login') }}?intended={{ url('/exhibition') }}" class="underline text-white">
                                 Please login to vote
                             </a>
