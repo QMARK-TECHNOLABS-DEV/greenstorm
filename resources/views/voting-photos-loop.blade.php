8fbf1a26215e8310ca80c50e1b22c9d487a7ae0e
badusha <83378824+badusha5618@users.noreply.github.com>
2025-05-21 17:57:08 +0530

    Update voting-photos-loop.blade.php

--- resources/views/voting-photos-loop.blade.php
+++ resources/views/voting-photos-loop.blade.php
@@ -22,7 +22,7 @@
                 @endif
 
                 <!-- Votes Count -->
-                <div class="absolute bottom-2 left-2 bg-black/60 text-white text-xs px-2 py-1 rounded">
+                <div class="absolute bottom-2 left-2 bg-black/60 text-black text-xs px-2 py-1 rounded">
                     Total Votes: {{ $voting->photograph->votes()->count() }}
                 </div>
 
