75beb6dfa539e802908e09699efb82c946c2cce2
badusha <83378824+badusha5618@users.noreply.github.com>
2025-06-05 18:38:05 +0530

    Update header.blade.php

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -69,7 +69,7 @@
                                     <i class='ms-1 fa fa-chevron-down'></i>
                                 </a>
                                 <ul class="dropdown-menu">
-                                    <!--<li class="nav-item"> <a href="{{ route('contest.voting') }}" class="nav-link"> Voting </a>   </li>-->
+                                 
                                     <li class="nav-item"> <a href="{{ route('contest.exhibition') }}" class="nav-link"> Exhibition </a>   </li>
                                      <li class="nav-item"> <a href="{{ route('getCamera') }}" class="nav-link"> Winners 2023 </a>   </li>
                                 </ul>
