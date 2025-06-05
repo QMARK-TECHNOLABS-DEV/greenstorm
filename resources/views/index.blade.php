26b834501ce2c2771e514393bb00162ddface532
badusha <83378824+badusha5618@users.noreply.github.com>
2025-06-05 18:07:14 +0530

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -160,7 +160,7 @@
 <!--                         <div id="carouselExample" class="carousel slide d-block d-md-none slider_mobile" data-bs-ride="carousel">
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <img src="{{ asset('web/img/main-banners/gs252.jpg') }}" class="banner_img w-100" alt="Slide 1">
+            <img src="{{ asset('web/img/main-banners/1234.jpg') }}" class="banner_img w-100" alt="Slide 1">
         </div>
         <div class="carousel-item">
             <img src="{{ asset('web/img/main-banners/gs254.jpg') }}" class="banner_img w-100" alt="Slide 2">
