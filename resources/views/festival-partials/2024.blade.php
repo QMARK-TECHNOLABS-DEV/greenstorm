3a904c60bf5ad3f171ae369d57c035965500f2b7
Ebrahimbadusha <83378824+badusha5618@users.noreply.github.com>
2025-09-09 13:11:42 +0530

    Update 2024.blade.php

--- resources/views/festival-partials/2024.blade.php
+++ resources/views/festival-partials/2024.blade.php
@@ -37,101 +37,101 @@
     </li>
 
     <li class="col-lg-4 col-md-6">
-        <a class="image-popup-vertical-fit" href="https://drive.google.com/file/d/13CbFPiLTZia836uSaCOBDl42tbYS-YJn/view?usp=sharing" title="ZHANG XU">
+        <a class="image-popup-vertical-fit" href="https://greenstorm.blob.core.windows.net/greenstormc/photo-uploads-2025/1738616677azr_Rice Fields.jpg" title="Saadet Banu Nazikcan">
             <div class='box-4'>
-                <figure><img src="https://drive.google.com/thumbnail?id=13CbFPiLTZia836uSaCOBDl42tbYS-YJn" alt="Zhang Xu" /></figure>
-                <div class='lupa text-center'><p class="phto_owner_name">ZHANG XU</p><p class="phto_owner_place">China</p></div>
+                <figure><img src="https://greenstorm.blob.core.windows.net/greenstormc/photo-uploads-2025/1738616677azr_Rice Fields.jpg" /></figure>
+                <div class='lupa text-center'><p class="phto_owner_name">Saadet Banu Nazikcan</p><p class="phto_owner_place">Thailand</p></div>
             </div>
         </a>
     </li>
 
     <li class="col-lg-4 col-md-6">
-        <a class="image-popup-vertical-fit" href="https://drive.google.com/file/d/1vpdWjFc_IFwQxpLKyUSVMBrnMZr922i6/view?usp=sharing" title="AMIN MALEKZADEH">
+        <a class="image-popup-vertical-fit" href="https://greenstorm.blob.core.windows.net/greenstormc/photo-uploads-2025/1738617062azr_BorÃ§ka KaragÃ¶l Artvin 3.jpg" title="Saadet Banu Nazikcan">
             <div class='box-4'>
-                <figure><img src="https://drive.google.com/thumbnail?id=1vpdWjFc_IFwQxpLKyUSVMBrnMZr922i6" alt="Amin Malekzadeh" /></figure>
-                <div class='lupa text-center'><p class="phto_owner_name">AMIN MALEKZADEH</p><p class="phto_owner_place">Iran</p></div>
+                <figure><img src="https://greenstorm.blob.core.windows.net/greenstormc/photo-uploads-2025/1738617062azr_BorÃ§ka KaragÃ¶l Artvin 3.jpg" alt="Saadet Banu Nazikcan" /></figure>
+                <div class='lupa text-center'><p class="phto_owner_name">Saadet Banu Nazikcan</p><p class="phto_owner_place">Thailand</p></div>
             </div>
         </a>
     </li>
 
     <li class="col-lg-4 col-md-6">
-        <a class="image-popup-vertical-fit" href="https://drive.google.com/file/d/1gc5MCnzulbZaBjewSV_jWoktlWvsiA0H/view?usp=sharing" title="ARJUN SASI">
+        <a class="image-popup-vertical-fit" href="https://greenstorm.blob.core.windows.net/greenstormc/photo-uploads-2025/1738649208azr_Ð£Ñ‚Ñ€Ð¾ Ð½Ð°  Ð·Ð°Ñ‚Ð¾ÐºÐ°Ñ….JPG" title="alexander">
             <div class='box-4'>
-                <figure><img src="https://drive.google.com/thumbnail?id=1gc5MCnzulbZaBjewSV_jWoktlWvsiA0H" alt="Arjun Sasi" /></figure>
-                <div class='lupa text-center'><p class="phto_owner_name">ARJUN SASI</p><p class="phto_owner_place">United Arab Emirates</p></div>
+                <figure><img src="https://greenstorm.blob.core.windows.net/greenstormc/photo-uploads-2025/1738649208azr_Ð£Ñ‚Ñ€Ð¾ Ð½Ð°  Ð·Ð°Ñ‚Ð¾ÐºÐ°Ñ….JPG" alt="alexander" /></figure>
+                <div class='lupa text-center'><p class="phto_owner_name">alexander</p><p class="phto_owner_place">Belarus</p></div>
             </div>
         </a>
     </li>
 
       <li class="col-lg-4 col-md-6">
-        <a class="image-popup-vertical-fit" href="https://drive.google.com/file/d/1gc5MCnzulbZaBjewSV_jWoktlWvsiA0H/view?usp=sharing" title="ARJUN SASI">
+        <a class="image-popup-vertical-fit" href="https://greenstorm.blob.core.windows.net/greenstormc/photo-uploads-2025/1738649641azr_Mirpal Tso S.jpg" title="Debdatta Chakraborty">
             <div class='box-4'>
-                <figure><img src="https://drive.google.com/thumbnail?id=1gc5MCnzulbZaBjewSV_jWoktlWvsiA0H" alt="Arjun Sasi" /></figure>
-                <div class='lupa text-center'><p class="phto_owner_name">ARJUN SASI</p><p class="phto_owner_place">United Arab Emirates</p></div>
+                <figure><img src="https://greenstorm.blob.core.windows.net/greenstormc/photo-uploads-2025/1738649641azr_Mirpal Tso S.jpg" alt="Debdatta Chakraborty" /></figure>
+                <div class='lupa text-center'><p class="phto_owner_name">Debdatta Chakraborty</p><p class="phto_owner_place">India</p></div>
             </div>
         </a>
     </li>
   <li class="col-lg-4 col-md-6">
-        <a class="image-popup-vertical-fit" href="https://drive.google.com/file/d/1gc5MCnzulbZaBjewSV_jWoktlWvsiA0H/view?usp=sharing" title="ARJUN SASI">
+        <a class="image-popup-vertical-fit" href="https://greenstorm.blob.core.windows.net/greenstormc/photo-uploads-2025/1738651922azr__MG_5206å¤§åœ°æ¬¢æ­ŒThe earth sings with joy.jpg" title="zhoukun">
             <div class='box-4'>
-                <figure><img src="https://drive.google.com/thumbnail?id=1gc5MCnzulbZaBjewSV_jWoktlWvsiA0H" alt="Arjun Sasi" /></figure>
-                <div class='lupa text-center'><p class="phto_owner_name">ARJUN SASI</p><p class="phto_owner_place">United Arab Emirates</p></div>
+                <figure><img src="https://greenstorm.blob.core.windows.net/greenstormc/photo-uploads-2025/1738651922azr__MG_5206å¤§åœ°æ¬¢æ­ŒThe earth sings with joy.jpg" alt="zhoukun" /></figure>
+                <div class='lupa text-center'><p class="phto_owner_name">zhoukun</p><p class="phto_owner_place">China</p></div>
             </div>
         </a>
     </li>
   <li class="col-lg-4 col-md-6">
-        <a class="image-popup-vertical-fit" href="https://drive.google.com/file/d/1gc5MCnzulbZaBjewSV_jWoktlWvsiA0H/view?usp=sharing" title="ARJUN SASI">
+        <a class="image-popup-vertical-fit" href="https://greenstorm.blob.core.windows.net/greenstormc/photo-uploads-2025/1738654557azr_IMG20210624185922_01-02.jpg" title="Krinjal Balkrishna Patil">
             <div class='box-4'>
-                <figure><img src="https://drive.google.com/thumbnail?id=1gc5MCnzulbZaBjewSV_jWoktlWvsiA0H" alt="Arjun Sasi" /></figure>
-                <div class='lupa text-center'><p class="phto_owner_name">ARJUN SASI</p><p class="phto_owner_place">United Arab Emirates</p></div>
+                <figure><img src="https://greenstorm.blob.core.windows.net/greenstormc/photo-uploads-2025/1738654557azr_IMG20210624185922_01-02.jpg" alt="Krinjal Balkrishna Patil" /></figure>
+                <div class='lupa text-center'><p class="phto_owner_name">Krinjal Balkrishna Patil</p><p class="phto_owner_place">India</p></div>
             </div>
         </a>
     </li>
   <li class="col-lg-4 col-md-6">
-        <a class="image-popup-vertical-fit" href="https://drive.google.com/file/d/1gc5MCnzulbZaBjewSV_jWoktlWvsiA0H/view?usp=sharing" title="ARJUN SASI">
+        <a class="image-popup-vertical-fit" href="https://greenstorm.blob.core.windows.net/greenstormc/photo-uploads-2025/1738662450azr_IMG_0348.jpeg" title="Suma Rachapudi">
             <div class='box-4'>
-                <figure><img src="https://drive.google.com/thumbnail?id=1gc5MCnzulbZaBjewSV_jWoktlWvsiA0H" alt="Arjun Sasi" /></figure>
-                <div class='lupa text-center'><p class="phto_owner_name">ARJUN SASI</p><p class="phto_owner_place">United Arab Emirates</p></div>
+                <figure><img src="https://greenstorm.blob.core.windows.net/greenstormc/photo-uploads-2025/1738662450azr_IMG_0348.jpeg" alt="Suma Rachapudi" /></figure>
+                <div class='lupa text-center'><p class="phto_owner_name">Suma Rachapudi</p><p class="phto_owner_place">India</p></div>
             </div>
         </a>
     </li>
   <li class="col-lg-4 col-md-6">
-        <a class="image-popup-vertical-fit" href="https://drive.google.com/file/d/1gc5MCnzulbZaBjewSV_jWoktlWvsiA0H/view?usp=sharing" title="ARJUN SASI">
+        <a class="image-popup-vertical-fit" href="https://greenstorm.blob.core.windows.net/greenstormc/photo-uploads-2025/1738663422azr_IMG_0361.jpeg" title="Suma Rachapudi">
             <div class='box-4'>
-                <figure><img src="https://drive.google.com/thumbnail?id=1gc5MCnzulbZaBjewSV_jWoktlWvsiA0H" alt="Arjun Sasi" /></figure>
-                <div class='lupa text-center'><p class="phto_owner_name">ARJUN SASI</p><p class="phto_owner_place">United Arab Emirates</p></div>
+                <figure><img src="https://greenstorm.blob.core.windows.net/greenstormc/photo-uploads-2025/1738663422azr_IMG_0361.jpeg" alt="Suma Rachapudi" /></figure>
+                <div class='lupa text-center'><p class="phto_owner_name">Suma Rachapudi</p><p class="phto_owner_place">India</p></div>
             </div>
         </a>
     </li>
   <li class="col-lg-4 col-md-6">
-        <a class="image-popup-vertical-fit" href="https://drive.google.com/file/d/1gc5MCnzulbZaBjewSV_jWoktlWvsiA0H/view?usp=sharing" title="ARJUN SASI">
+        <a class="image-popup-vertical-fit" href="https://greenstorm.blob.core.windows.net/greenstormc/photo-uploads-2025/1738663551azr_DJI_0915-1.jpg" title="Azim Khan Ronnie">
             <div class='box-4'>
-                <figure><img src="https://drive.google.com/thumbnail?id=1gc5MCnzulbZaBjewSV_jWoktlWvsiA0H" alt="Arjun Sasi" /></figure>
-                <div class='lupa text-center'><p class="phto_owner_name">ARJUN SASI</p><p class="phto_owner_place">United Arab Emirates</p></div>
+                <figure><img src="https://drive.google.com/thumbnail?id=1gc5MCnzulbZaBjewSV_jWoktlWvsiA0H" alt="Azim Khan Ronnie" /></figure>
+                <div class='lupa text-center'><p class="phto_owner_name">Azim Khan Ronnie</p><p class="phto_owner_place">France</p></div>
             </div>
         </a>
     </li>
   <li class="col-lg-4 col-md-6">
-        <a class="image-popup-vertical-fit" href="https://drive.google.com/file/d/1gc5MCnzulbZaBjewSV_jWoktlWvsiA0H/view?usp=sharing" title="ARJUN SASI">
+        <a class="image-popup-vertical-fit" href="https://greenstorm.blob.core.windows.net/greenstormc/photo-uploads-2025/1738663883azr_DJI_0566.jpg" title="Azim Khan Ronnie">
             <div class='box-4'>
-                <figure><img src="https://drive.google.com/thumbnail?id=1gc5MCnzulbZaBjewSV_jWoktlWvsiA0H" alt="Arjun Sasi" /></figure>
-                <div class='lupa text-center'><p class="phto_owner_name">ARJUN SASI</p><p class="phto_owner_place">United Arab Emirates</p></div>
+                <figure><img src="https://greenstorm.blob.core.windows.net/greenstormc/photo-uploads-2025/1738663883azr_DJI_0566.jpg" alt="Azim Khan Ronnie" /></figure>
+                <div class='lupa text-center'><p class="phto_owner_name">Azim Khan Ronnie</p><p class="phto_owner_place">France</p></div>
             </div>
         </a>
     </li>
   <li class="col-lg-4 col-md-6">
-        <a class="image-popup-vertical-fit" href="https://drive.google.com/file/d/1gc5MCnzulbZaBjewSV_jWoktlWvsiA0H/view?usp=sharing" title="ARJUN SASI">
+        <a class="image-popup-vertical-fit" href="https://greenstorm.blob.core.windows.net/greenstormc/photo-uploads-2025/1738664280azr__K6A8720 copy.jpg" title="Mainak Ray">
             <div class='box-4'>
-                <figure><img src="https://drive.google.com/thumbnail?id=1gc5MCnzulbZaBjewSV_jWoktlWvsiA0H" alt="Arjun Sasi" /></figure>
-                <div class='lupa text-center'><p class="phto_owner_name">ARJUN SASI</p><p class="phto_owner_place">United Arab Emirates</p></div>
+                <figure><img src="https://greenstorm.blob.core.windows.net/greenstormc/photo-uploads-2025/1738664280azr__K6A8720 copy.jpg" alt="Mainak Ray" /></figure>
+                <div class='lupa text-center'><p class="phto_owner_name">Mainak Ray</p><p class="phto_owner_place">India</p></div>
             </div>
         </a>
     </li>
   <li class="col-lg-4 col-md-6">
-        <a class="image-popup-vertical-fit" href="https://drive.google.com/file/d/1gc5MCnzulbZaBjewSV_jWoktlWvsiA0H/view?usp=sharing" title="ARJUN SASI">
+        <a class="image-popup-vertical-fit" href="https://greenstorm.blob.core.windows.net/greenstormc/photo-uploads-2025/1738664440azr_DJI_0390.jpg" title="Azim Khan Ronnie">
             <div class='box-4'>
-                <figure><img src="https://drive.google.com/thumbnail?id=1gc5MCnzulbZaBjewSV_jWoktlWvsiA0H" alt="Arjun Sasi" /></figure>
-                <div class='lupa text-center'><p class="phto_owner_name">ARJUN SASI</p><p class="phto_owner_place">United Arab Emirates</p></div>
+                <figure><img src="https://greenstorm.blob.core.windows.net/greenstormc/photo-uploads-2025/1738664440azr_DJI_0390.jpg" alt="Azim Khan Ronnie" /></figure>
+                <div class='lupa text-center'><p class="phto_owner_name">Azim Khan Ronnie</p><p class="phto_owner_place">France</p></div>
             </div>
         </a>
     </li>
